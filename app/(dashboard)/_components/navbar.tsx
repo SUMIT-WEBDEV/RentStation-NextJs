c0d467c47b170c1cebe2c3ce939e48102c959ea3
Sumit Sahu <sahu99516@gmail.com>
2024-03-03 00:37:57 +0530

    feat: integrate location api

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -27,6 +27,8 @@ const dummySuggestions = [
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import { useRouter } from "next/navigation";
+import { ADDRESS_API, CORSPROXY, SEARCH_LOCATION_API } from "@/lib/constant";
+import { Search } from "lucide-react";
 
 function Navbar() {
   const locations = ["Delhi", "Mumbai", "Hyderabad"];
@@ -34,6 +36,11 @@ function Navbar() {
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [accountSidebar, setAccountSidebar] = useState(false);
   const { setTheme } = useTheme();
+
+  const [Locations, setLocations] = useState([])
+  const [SearchText, setSearchText] = useState("")
+
+
   const router = useRouter();
 
   useEffect(() => {
@@ -44,40 +51,58 @@ function Navbar() {
     setSuggestions(filteredSuggestions);
   }, [query]);
 
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://countriesnow.space/api/v0.1/countries/cities",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              country: "india",
-            }),
-          }
-        );
 
+
+  const handleSearchLocation = async (e: any) => {
+    try {
+      setSearchText(e.target.value);
+      if (SearchText.length >= 3) {
+        const response = await fetch(CORSPROXY + encodeURIComponent(SEARCH_LOCATION_API) + SearchText)
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          // const err = response.status;
+          // throw new err
+          console.log("err")
         }
-
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.log("error");
+        else {
+          const res = await response.json();
+          console.log("location", res?.data)
+          setLocations(res?.data)
+        }
       }
-    };
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-    fetchData();
-  }, []);
 
-  // console.log("data is", data);
+  const handleUserLocation = async (placeid: string) => {
+    try {
+      const response = await fetch(CORSPROXY + encodeURIComponent(ADDRESS_API) + placeid)
+      if (!response.ok) {
+        const err = response.status;
+        throw new Error("err")
+      }
+      else {
+        const { data } = await response.json();
+        // dispatch(getLocation({
+        //     city: data[0]?.address_components[0]?.short_name,
+        //     lat: data[0]?.geometry?.location?.lat,
+        //     lng: data[0]?.geometry?.location?.lng,
+        //     address: data[0]?.formatted_address
+        // }))
+        console.log("data return", data)
+      }
+      // window.location.reload();
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+
 
   const handleCloseSidebar = () => {
     setAccountSidebar(false);
@@ -88,10 +113,11 @@ function Navbar() {
   };
 
   return (
-    <div className="sticky overflow-hidden">
-      <div className="lg:flex lg:flex-row justify-around items-center bg-black text-white p-4 relative w-screen">
-        <div className="flex lg:items-center lg:flex-row flex-col ">
-          <div className="mb-2 lg:mr-6 flex justify-between ">
+    <div className="sticky">
+      <div className="lg:flex lg:flex-row justify-around items-center bg-black text-white p-4 w-screen">
+        <div className="flex lg:items-center lg:flex-row flex-col">
+
+          <div className="lg:mb-0 mb-2 lg:mr-6 flex justify-between ">
             <Link
               href="/settings"
               className="text-xl text-yellow-500 font-extrabold tracking-normal lg:tracking-wider"
@@ -104,40 +130,65 @@ function Navbar() {
           </div>
 
           {/* Search Form */}
-          <div className="flex items-center">
-            <form className="mr-4 lg:block hidden">
-              <select className="bg-gray-400 text-white rounded-md px-3 py-2 outline-none h-9">
-                <option>--Select City--</option>
-                {locations?.map((city, index) => (
-                  <option key={index}>{city}</option>
-                ))}
-              </select>
-            </form>
+          <div className="flex items-center lg:space-x-2">
 
-            <div className="flex items-center bg-gray-400 rounded-md relative w-full border border-white focus:border-gray-700 lg:border-none justify-between">
+
+            {/* <div className="lg:block hidden relative w-52 bg-slate-50 border rounded-md"> */}
+            <div className=" lg:flex items-center hidden relative w-60 min-w-auto  rounded-md focus:border-gray-700 lg:border-none justify-between bg-slate-50 ">
+
               <input
-                className="px-3 py-2 h-9 placeholder:text-white bg-gray-400 rounded-md outline-none "
+                className="px-3 py-2 h-9 outline-none rounded-md bg-transparent text-black text-sm"
+                type="text"
+                onChange={(e) => handleSearchLocation(e)} value={SearchText}
+                placeholder="Select Location"
+              />
+
+              <LocationOnIcon className="text-gray-500 absolute right-3" />
+
+              {SearchText.length > 0 && (
+                <ul className="absolute text-xs top-full w-full bg-slate-50 text-black rounded-md mt-2 shadow-lg border-gray-300 border">
+                  {
+                    Locations?.map((item: any) => (
+                      <div className="text-xs p-2 cursor-pointer" key={item?.place_id} onClick={() => handleUserLocation(item?.place_id)}>
+                        <p className='font-ProximaNovaMed text-color-1'>{item?.structured_formatting?.main_text}</p>
+                        <p className='text-color-5 leading-5 font-ProximaNovaThin'>{item?.structured_formatting?.secondary_text}</p>
+                      </div>
+                    ))
+                  }
+                </ul>
+              )}
+
+            </div>
+
+            <div className="flex items-center relative w-full bg-red lg:w-80 min-w-auto border rounded-md border-white focus:border-gray-700 lg:border-none justify-between bg-slate-50">
+              <input
+                className="px-3 py-2 h-9 outline-none rounded-md bg-transparent text-black text-sm"
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 placeholder="Search Your Product..."
               />
+
+              <Search className="absolute right-3 text-gray-500"
+                onClick={handleSearch}
+              />
+
               {query.length > 0 && suggestions.length > 0 && (
-                <ul className="absolute z-50 top-full w-full bg-gray-200 border text-black rounded-md shadow-sm">
+                <ul className="absolute z-50 top-full w-full bg-slate-50 border-gray-300 border text-black rounded-md py-2 mt-2 shadow-lg">
                   {suggestions.map((suggestion, index) => (
-                    <li key={index} className="px-4 py-2">
-                      {suggestion}
+                    <li key={index} className="px-4 border-gray-400 text-sm">
+                      <p className="pt-2">
+                        {suggestion}
+                      </p>
                     </li>
                   ))}
                 </ul>
               )}
-              <div
-                className="bg-gray-400 w-4 h-4 flex items-center justify-center rounded-md p-4 cursor-pointer"
-                onClick={handleSearch}
-              >
-                🔍
-              </div>
+
             </div>
+
+
+
           </div>
         </div>
 
@@ -186,8 +237,77 @@ function Navbar() {
         </div>
         {accountSidebar && <Sidebar onClose={handleCloseSidebar} />}
       </div>
-    </div>
+    </div >
   );
 }
 
 export default Navbar;
+
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const response = await fetch(
+//         "https://countriesnow.space/api/v0.1/countries/cities",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify({
+//             country: "india",
+//           }),
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+
+//       const result = await response.json();
+//       setData(result);
+//     } catch (error) {
+//       console.log("error");
+//     }
+//   };
+
+//   fetchData();
+// }, []);
+
+// console.log("data is", data);
+
+
+
+{/* <ul className="absolute top-full w-full bg-slate-50 border text-black rounded-md shadow-sm">
+{
+  SearchText && Locations?.map((item: any) => (
+    <li onClick={() => handleUserLocation(item?.place_id)} key={item?.place_id} className='cursor-pointer relative'>
+    <li onClick={() => { }} className='cursor-pointer relative'>
+      <div className='md:p-6 py-4 flex location'>
+        <div className='text-lg text-color-6 w-8 text-left pt-1 pr-4'>
+          <GoLocation />
+          📍
+        </div>
+        <div className='flex flex-col'>
+          <h3 className='text-base font-ProximaNovaMed text-color-1'>{item?.structured_formatting?.main_text}</h3>
+          <h4 className='text-[13px] text-color-5 leading-5 font-ProximaNovaThin'>{item?.structured_formatting?.secondary_text}</h4>
+        </div>
+      </div>
+    </li>
+  ))
+}
+</ul> */}
+
+
+
+// {
+//   query.length > 0 && suggestions.length > 0 && (
+//     <ul className="absolute top-full w-full bg-slate-50 border text-black rounded-md shadow-sm">
+//       {suggestions.map((suggestion, index) => (
+//         <li key={index} className="px-4 py-2">
+//           {suggestion}
+//         </li>
+//       ))}
+//     </ul>
+//   )
+// }
---
+++ lib/constant.ts
@@ -0,0 +1,7 @@
+export const CORSPROXY = "https://corsproxy.org/?";
+
+export const SEARCH_LOCATION_API =
+  "https://www.swiggy.com/dapi/misc/place-autocomplete?input=";
+
+export const ADDRESS_API =
+  "https://www.swiggy.com/dapi/misc/address-recommend?place_id=";
