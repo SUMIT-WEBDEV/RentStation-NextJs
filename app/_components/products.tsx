996665fe5dc92849e88c688b9b462133a47ef2b6
Sumit Sahu <71073768+SUMIT-WEBDEV@users.noreply.github.com>
2024-03-27 01:40:55 +0530

    Update products.tsx

--- app/_components/products.tsx
+++ app/_components/products.tsx
@@ -1,106 +1,30 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { ProductCard } from "./product-card";
-import { ProductSkeleton } from "./product-skelton";
+import { Suspense } from "react"
+import Products from "./products";
 import { getProducts } from "@/actions/get-product";
-import useStoreLocation from "@/store/user-location";
-
-
-type Product = {
-  id: string;
-  location: string;
-  price: string;
-  title: string;
-  duration: string;
-  category: string;
-  image?: string | null;
-  userId: string;
-  createdAt: Date;
-};
-
-type ProductsProps = {
-  products: Product[];
-  user: any
-  // loadingproduct: boolean
-  // isLocation: string
-};
-
-export function Products({ products, user }: ProductsProps) {
-  const [page, setPage] = useState<number>(1);
-  const [productData, setProductData] = useState<Product[]>(products)
-  // const [loading, setLoading] = useState<boolean>(loadingproduct);
-  // const { storedLocation, storeLocation, setLocation } = useStoreLocation();
-  // const [initialProducts, setInitialProducts] = useState<Product[]>([])
-  // const [loadingproduct, setloadingproduct] = useState(true)
-
-
-  const { storedLocation, storeLocation, setLocation } = useStoreLocation();
-  const isLocation = storedLocation?.city
-
-
-  useEffect(() => {
-    // console.log("I rendered on feed")
-    const fetchInitialProduct = async () => {
-      const products = await getProducts({ location: isLocation });
-      setProductData(products)
-    }
-    fetchInitialProduct()
-  }, [isLocation])
+import { ProductSkeleton } from "./product-skelton";
 
 
 
-  useEffect(() => {
-    setProductData(products);
-    // setLoading(false);
-  }, [products]);
+async function Home({ user, location }: any) {
 
-
-  const loadMoreProducts = async () => {
-    const next: number = page + 1;
-    const products = await getProducts({ page: next, location: isLocation });
-    setPage(next)
-    setProductData([...productData, ...products])
-  }
-
-  const loading = false
+  const initialProducts = await getProducts({ location });
 
   return (
-    <div className="lg:m-3 w-full">
-      <div className="flex justify-center w-full">
-        <div className="w-full lg:w-auto mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-          {loading
-            ?
-            Array.from({ length: 8 }).map((_, index) => <ProductSkeleton key={index} />)
+    <div className="flex flex-col mt-5 w-full">
+      {/* TODO: favourite  */}
 
-            : products.length === 0 ? <div>No products found</div> : (
-              productData.map((product: Product) => {
-
-
-                const isFavorite = user && user?.favorites && user?.favorites.some((fav: any) => fav.productId === product.id) || false
-
-                // console.log("isFavorite is", isFavorite)
-
-                return (
-                  <ProductCard key={product.id} product={product} isFavorite={isFavorite} />)
-              }
-              )
-            )
-          }
+      {/* TODO: Trending  */}
+      <div className="mx-auto">
+        <h1 className="lg:text-3xl text-xl py-2 pl-1 font-medium text-gray-900">
+          Fresh Recommendations
+        </h1>
+        <div className="flex justify-center items-center">
+       
+            <Products products={initialProducts} user={user} />
         </div>
-      </div>
-      <div className="flex mt-3">
-        <Button onClick={loadMoreProducts} className="m-auto">Load More</Button>
       </div>
     </div>
   );
 }
 
-export default Products;
-
-
-
-
+export default Home;
