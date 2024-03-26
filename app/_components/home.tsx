5f4282dd5e8e8e879a011537e6606b63af8e516e
Sumit Sahu <sahu99516@gmail.com>
2024-03-27 00:39:27 +0530

    chore: added suspense fallback

--- app/_components/home.tsx
+++ app/_components/home.tsx
@@ -1,40 +1,43 @@
-
-import React from "react";
+import { Suspense } from "react"
 import Products from "./products";
 import { getProducts } from "@/actions/get-product";
+import { ProductSkeleton } from "./product-skelton";
 
-type Product = {
-  id: string;
-  location: string;
-  price: string;
-  title: string;
-  duration: string;
-  category: string;
-  image?: string | null;
-  userId: string;
-  createdAt: Date;
-};
-
-
-async function Home({ user, location }: any) {
-
+async function HomeSuspense({ user, location }: any) {
   const initialProducts = await getProducts({ location });
 
   return (
-    <div className="flex flex-col mt-5 w-full">
-      {/* TODO: favourite  */}
+    <Products products={initialProducts} user={user} />
+  );
+}
 
-      {/* TODO: Trending  */}
+const Home = ({ user, location }: any) => {
+  return (
+    <div className="flex flex-col mt-5 w-full">
       <div className="mx-auto">
         <h1 className="lg:text-3xl text-xl py-2 pl-1 font-medium text-gray-900">
           Fresh Recommendations
         </h1>
         <div className="flex justify-center items-center">
-          <Products products={initialProducts} user={user} />
+
+          <Suspense fallback={<div className="w-full lg:w-auto mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+          </div>
+          }>
+            <HomeSuspense user={user} location={location} />
+          </Suspense>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
+
