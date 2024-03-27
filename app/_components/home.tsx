c4aeda142801e1fa19e367b020719b92e68ca609
Sumit Sahu <71073768+SUMIT-WEBDEV@users.noreply.github.com>
2024-03-27 09:45:03 +0530

    Update home.tsx

--- app/_components/home.tsx
+++ app/_components/home.tsx
@@ -1,30 +1,58 @@
+
+
 import { Suspense } from "react"
 import Products from "./products";
 import { getProducts } from "@/actions/get-product";
 import { ProductSkeleton } from "./product-skelton";
+import { cookies } from 'next/headers';
+import { currentUserDetails } from "@/lib/auth";
 
 
+async function HomeSuspense() {
 
-async function Home({ user, location }: any) {
+  const user = await currentUserDetails()
+  const nextCookies = cookies().get('userLocation');
+  const address = nextCookies ? JSON.parse(nextCookies.value).city : "";
 
-  const initialProducts = await getProducts({ location });
+  console.log("address hai", address)
+
+
+  const initialProducts = await getProducts({ location: address });
 
   return (
-    <div className="flex flex-col mt-5 w-full">
-      {/* TODO: favourite  */}
+    <Products products={initialProducts} user={user} />
+  );
+}
 
-      {/* TODO: Trending  */}
+const Home = () => {
+  return (
+    <div className="flex flex-col mt-5 w-full">
       <div className="mx-auto">
         <h1 className="lg:text-3xl text-xl py-2 pl-1 font-medium text-gray-900">
           Fresh Recommendations
         </h1>
         <div className="flex justify-center items-center">
-       
-            <Products products={initialProducts} user={user} />
+
+          <Suspense fallback={<div className="w-full lg:w-auto mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+          </div>
+          }>
+            <HomeSuspense />
+          </Suspense>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
+
+
+
