40dad7ec290c52f0bc062c3ecf37717effca532a
Sumit Sahu <sahu99516@gmail.com>
2024-03-27 23:16:15 +0530

    fix: props error on category card

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,13 @@ import React from "react";
 
 import CategoryCard from "./_components/category/category-card";
 import Home from "./_components/home";
-import { currentUserDetails } from "@/lib/auth";
-import { cookies } from 'next/headers';
 
 async function page() {
-
-  const nextCookies = cookies().get('userLocation');
-  const address = nextCookies ? JSON.parse(nextCookies.value).city : "";
 
   return (
     <div className=" w-full items-center flex justify-center bg-slate-50 flex-col">
       <div className="lg:w-11/12 w-full">
-        <CategoryCard address={address} />
+        <CategoryCard />
         <Home />
       </div>
     </div>
@@ -23,11 +18,3 @@ async function page() {
 export default page;
 
 
-
-// https://github.com/SUMIT-WEBDEV/RentStation-NextJs
-// https://github.com/settings/applications/2433325
-// https://console.cloud.google.com/apis/credentials?project=next-auth-410307
-// https://rent-station-next-js.vercel.app
-
-// TODO:
-// - wishlist check login or not
