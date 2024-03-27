f3e4e26b1651cf6dd36fcd786e8f976972e421fd
Sumit Sahu <sahu99516@gmail.com>
2024-03-27 23:12:10 +0530

    fix: category-card glitch on ui

--- app/_components/category/category-card.tsx
+++ app/_components/category/category-card.tsx
@@ -1,29 +1,31 @@
 "use client"
-import React, { useState } from "react";
+
+import React from "react";
 import Link from "next/link";
+import useStoreLocation from "@/store/user-location";
 import { iconData } from "./category-data";
 
-function CategoryCard({ address }: any) {
-  const [userLocation, setUserLocation] = useState(address);
+function CategoryCard() {
+  const { storedLocation } = useStoreLocation();
+  const userLocation = storedLocation?.city
+
 
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="grid grid-cols-4 lg:grid-cols-8 gap-2 justify-center py-3 lg:w-auto w-11/12">
+      <div className="grid grid-cols-4 lg:grid-cols-8 gap-2 justify-center py-3 lg:w-auto w-full px-2">
         {iconData.map((item, index) => (
           <Link
             key={index}
-            href={`/${userLocation ? userLocation + "/" : ""}${item.text}`}
+            // onClick={() => handleItemClick(item.text)}
+            href={userLocation ? `/${userLocation}/${item.text}` : `/${item.text}`}
+            className="lg:w-32 min-w-16 w-auto flex flex-col lg:p-3 lg:border lg:border-gray-300 rounded-md justify-between cursor-pointer lg:hover:shadow-md"
           >
-            <a>
-              <div className="lg:w-32 min-w-16 w-auto flex flex-col lg:p-3 lg:border lg:border-gray-300 rounded-md justify-between cursor-pointer lg:hover:shadow-md">
-                <div className="text-center">{item.icon}</div>
-                <div className="lg:h-9 h-6">
-                  <p className="text-center lg:text-xs text-[0.5rem] mt-2">
-                    {item.text}
-                  </p>
-                </div>
-              </div>
-            </a>
+            <div className="text-center">{item.icon}</div>
+            <div className="lg:h-9 h-6">
+              <p className="text-center lg:text-xs text-[0.5rem] mt-2">
+                {item.text}
+              </p>
+            </div>
           </Link>
         ))}
       </div>
