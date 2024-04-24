46ba21b59aacec0af347a7d1560fe2190924a1c5
Sumit Sahu <sahu99516@gmail.com>
2024-04-24 11:56:51 +0530

    refactor: not found component

--- app/_components/not-found-product.tsx
+++ app/_components/not-found-product.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 const NotFound = () => {
     return (
-        <div className="h-[60vh] flex items-center justify-center">
+        <div className="h-[60vh] flex items-center justify-center lg:p-0 px-20 ">
             <div>
-                <Image src={NotFoundImage} width={350} height={350} alt="" />
-                <h2 className="text-center tracking-wide font-semibold text-lg text-#242424">No Products found matches this search!</h2>
+                <Image src={NotFoundImage} width={350} height={350} alt="" className='opacity-95' />
+                <h2 className="text-center tracking-wide font-semibold text-lg text-[#242424]">No Products found matches this search!</h2>
             </div>
         </div>
     )
