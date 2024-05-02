119b0a92955eb589a879448534fa26c1de0cdfda
Sumit Sahu <sahu99516@gmail.com>
2024-05-02 17:32:10 +0530

    feat: upload location from the api in product form component

--- app/_components/product-form.tsx
+++ app/_components/product-form.tsx
@@ -19,14 +19,32 @@ import { FormSuccess } from "@/components/form-success";
 import { createProduct } from "@/actions/create-product";
 import { ProductSchema } from "@/schemas";
 import { useCurrentUser } from "@/hooks/use-current-user";
+import { ADDRESS_API, SEARCH_LOCATION_API } from "@/lib/constant";
+import { useRouter } from "next/navigation";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
 // import { CldImage } from "next-cloudinary";
 
 function ProductForm() {
   const user = useCurrentUser();
-
-  // console.log("user is", user?.id);
+  const router = useRouter();
 
   const [isPending, startTransition] = useTransition();
+
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [selectedImage, setSelectedImage] = useState<File | undefined>(
+    undefined
+  );
+
+  const CORSPROXY = process.env.NEXT_PUBLIC_CORSPROXY
+  const [showSuggestion, setShowSuggestion] = useState(false)
+  const [locations, setLocations] = useState([])
+  const [sellerLocation, setSellerLocation] = useState<string>("")
+  const [SearchText, setSearchText] = useState<string>(sellerLocation || "")
+
+
   const form = useForm<z.infer<typeof ProductSchema>>({
     resolver: zodResolver(ProductSchema),
     defaultValues: {
@@ -40,11 +58,6 @@ function ProductForm() {
     },
   });
 
-  const [error, setError] = useState<string | undefined>("");
-  const [success, setSuccess] = useState<string | undefined>("");
-  const [selectedImage, setSelectedImage] = useState<File | undefined>(
-    undefined
-  );
 
   const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -68,11 +81,12 @@ function ProductForm() {
         body: formData,
       }
     );
+
     const uploadedImageData = await uploadResponse.json();
     const imageUrl = uploadedImageData.secure_url;
 
     const serializableValues = {
-      location: values.location,
+      location: sellerLocation,
       price: values.price,
       description: values.description,
       title: values.title,
@@ -87,22 +101,68 @@ function ProductForm() {
         setSuccess(data.success);
 
         if (!data.error) {
-          form.reset(); // Reset the form values
+          form.reset();
           setSelectedImage(undefined);
         }
       });
     });
   };
 
+  const handleSearchLocation = async (e: any) => {
+    try {
+      setSearchText(e.target.value);
+      setShowSuggestion(true)
+      if (SearchText.length >= 3) {
+        const response = await fetch(CORSPROXY + SEARCH_LOCATION_API + SearchText)
+        if (!response.ok) {
+          console.log("err")
+        }
+        else {
+          const res = await response.json();
+          console.log("res", res.data)
+          setLocations(res?.data)
+        }
+      }
+    } catch (error) {
+      console.log("error is", error)
+    }
+  }
+
+  const handleUserLocation = async (placeid: string) => {
+    try {
+      const response = await fetch(CORSPROXY + ADDRESS_API + placeid)
+      if (!response.ok) {
+        const err = response.status;
+        throw new Error("err")
+      }
+      else {
+        const { data } = await response.json();
+        setSellerLocation(data[0]?.formatted_address)
+        router.refresh()
+        setSearchText(data[0]?.formatted_address)
+        setShowSuggestion(false)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleCloseLocationBar = () => {
+    setShowSuggestion(false)
+    setSearchText(sellerLocation || '')
+  }
+
   return (
     <div className="w-full flex justify-center h-screen">
       <div className="lg:w-3/5 w-full m-4">
         <h1 className="p-1 font-bold text-xl">Post Your Ad</h1>
+
         <div className="p-5 border border-gray-300 rounded-md">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
               <div className="space-y-4">
                 <>
+
                   <FormField
                     control={form.control}
                     name="location"
@@ -113,14 +173,43 @@ function ProductForm() {
                           <Input
                             {...field}
                             // disabled={isPending}
-                            placeholder="location"
                             type="text"
+                            onChange={(e) => {
+                              field.onChange(e); // Make sure to propagate the onChange event to react-hook-form
+                              handleSearchLocation(e); // Also handle your custom logic
+                            }}
+                            value={SearchText}
+                            onFocus={() => setSearchText('')}
+                            onBlur={handleCloseLocationBar}
+                            placeholder="Select Location"
                           />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
                     )}
                   />
+
+                  <div>
+                    {/* <LocationOnIcon className="text-gray-500 right-3" /> */}
+
+                    {showSuggestion && locations.length > 0 && (
+                      <ul className="relative text-xs top-full w-full bg-slate-50 text-black rounded-md mt-2 shadow-lg border-gray-300 border" id="badButton"
+                        onMouseDown={(e) => e.preventDefault()}
+                      >
+                        {
+                          locations?.map((item: any) => (
+                            <div className="text-xs p-2 cursor-pointer" key={item?.place_id} onClick={() => handleUserLocation(item?.place_id)}>
+                              {/* <LocationOnIcon className="text-gray-500" /> */}
+                              <p className='font-ProximaNovaMed text-color-1'>{item?.structured_formatting?.main_text}</p>
+                              <p className='text-color-5 leading-5 font-ProximaNovaThin'>{item?.structured_formatting?.secondary_text}</p>
+                            </div>
+                          ))
+                        }
+                      </ul>
+                    )}
+                  </div>
+
+
                   <FormField
                     control={form.control}
                     name="price"
@@ -241,6 +330,24 @@ function ProductForm() {
           </Form>
         </div>
       </div>
+
+      {showSuggestion && locations.length > 0 && (
+        <ul className="absolute text-xs top-full w-full bg-slate-50 text-black rounded-md mt-2 shadow-lg border-gray-300 border" id="badButton"
+          onMouseDown={(e) => e.preventDefault()}
+        >
+          {
+            locations?.map((item: any) => (
+              <div className="text-xs p-2 cursor-pointer" key={item?.place_id} onClick={() => handleUserLocation(item?.place_id)}>
+                {/* <LocationOnIcon className="text-gray-500" /> */}
+                <p className='font-ProximaNovaMed text-color-1'>{item?.structured_formatting?.main_text}</p>
+                <p className='text-color-5 leading-5 font-ProximaNovaThin'>{item?.structured_formatting?.secondary_text}</p>
+              </div>
+            ))
+          }
+        </ul>
+      )}
+
+
     </div>
   );
 }
